cee624697dc03f8ffdadad2fa74e4848ad03dec7
Keita Kobayashi <keita@kkob.us>
2015-04-19 00:19:17 +0900

    More work on the downloader

--- lib/msd/downloader/supervisor.ex
+++ lib/msd/downloader/supervisor.ex
@@ -5,13 +5,13 @@ defmodule MSD.Downloader.Supervisor do
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  def start_download(uri) do
-    Supervisor.start_child(__MODULE__, [[uri: uri]])
+  def start_download(uri, save_to) do
+    Supervisor.start_child(__MODULE__, [%{uri: uri, save_to: save_to}])
   end
 
   def init(:ok) do
     children = [
-      worker(MSD.Downloader.Worker, [])
+      worker(MSD.Downloader.Worker, [], restart: :transient)
     ]
 
     supervise(children, strategy: :simple_one_for_one)
--- lib/msd/downloader/worker.ex
+++ lib/msd/downloader/worker.ex
@@ -1,19 +1,57 @@
 defmodule MSD.Downloader.Worker do
   use GenServer
 
+  import HTTPoison, only: [get: 3]
+
+  @doc """
+  Timeout of the HTTP connection in ms. Default 1000ms (1s)
+  """
+  @timeout 1000
+
   @doc """
   Starts the worker.
   """
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, [
-      uri: opts[:uri]
-    ], opts)
+  def start_link(%{uri: _, save_to: _} = state, opts \\ []) do
+    GenServer.start_link(__MODULE__, state, opts)
   end
 
   ## Server Callbacks
 
   def init(opts) do
     IO.puts "Starting download of #{opts[:uri]}..."
-    {:ok, %{uri: opts[:uri]}}
+    get opts[:uri], [], timeout: @timeout, stream_to: self
+
+    {:ok, %{uri: opts[:uri], save_to: opts[:save_to]}}
+  end
+
+  def handle_info(%HTTPoison.AsyncStatus{code: 200}, state) do
+    IO.puts "Started download."
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncStatus{code: error}, state) do
+    # Ignore an error.
+    IO.puts "Got #{error}, exiting."
+    {:stop, :normal, state}
+  end
+
+  # Ignore headers for now.
+  def handle_info(%HTTPoison.AsyncHeaders{headers: _}, state) do
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncChunk{chunk: data}, state) do
+    IO.puts "Got a chunk (#{byte_size data}b)."
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncEnd{}, state) do
+    IO.puts "Finished #{state[:uri]}."
+    {:stop, :normal, state}
+  end
+
+  def handle_info(msg, state) do
+    IO.puts "Received: #{inspect msg}"
+    {:noreply, state}
   end
 end
